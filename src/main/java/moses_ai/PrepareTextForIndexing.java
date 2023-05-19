663fd53acec1715a30114dac67772629375db85f
Mark Kerzner <mark@elephantscale.com>
2023-05-19 18:30:39 -0500

    code

--- src/main/java/moses_ai/PrepareTextForIndexing.java
+++ src/main/java/moses_ai/PrepareTextForIndexing.java
@@ -1,11 +1,10 @@
 package moses_ai;
 
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -27,32 +26,31 @@ public class PrepareTextForIndexing {
                 if (dir.isDirectory()) {
                     File[] listOfFiles = dir.listFiles();
                     for (File file : listOfFiles)
-                        if (file.isFile() && file.getName().endsWith(".html")) {
-                            Document doc = Jsoup.parse(file, "UTF-8");
-                            String htmlWithLineBreaks = doc.html().replaceAll("(?i)<br[^>]*>", "br2n");
-                            Document docWithLineBreaks = Jsoup.parse(htmlWithLineBreaks, "UTF-8");
-                            String text = docWithLineBreaks.body().text(); // extract text
-                            text = text.replaceAll("br2n", "\n");
-                            String[] paragraphs = text.split("\n+"); // split into paragraphs
+                    if (file.isFile() && file.getName().endsWith(".html")) {
+                        Document doc = Jsoup.parse(file, "UTF-8");
+                        String htmlWithLineBreaks = doc.html().replaceAll("(?i)<br[^>]*>", "br2n");
+                        Document docWithLineBreaks = Jsoup.parse(htmlWithLineBreaks, "UTF-8");
+                        String text = docWithLineBreaks.body().text(); // extract text
+                        text = text.replaceAll("br2n", "\n");
+                        String[] paragraphs = text.split("\n+"); // split into paragraphs
+                        // output each paragraph into a separate file in the output director
+                        int paragraphCount = 0;
+                        for (int i = 0; i < paragraphs.length; i++) {
+                            String paragraphFile = OUTPUT_DIR + "/" + file.getName() + "-paragraph-" + paragraphCount + ".txt";
 
-                            // output each paragraph into a separate file in the output director
-                            int paragraphCount = 0;
-                            for (int i = 0; i < paragraphs.length; i++) {
-                                String paragraphFile = OUTPUT_DIR + "/" + file.getName() + "-paragraph-" + paragraphCount + ".txt";
-                                try (BufferedWriter writer = new BufferedWriter(new FileWriter(paragraphFile))) {
-                                    String paragraph = paragraphs[i];
-                                    if (paragraph.length() > 3 && // skip empty paragraph
-                                            !(paragraph.startsWith("Art")) && // skip description of art in summary
-                                            !(paragraph.startsWith(" Art")) && // skip description of art in summary
-                                            !paragraph.startsWith("Leave a comment") // skip "Leave a comment"
-                                    ) {
-                                        System.out.println("Writing: " + paragraph);
-                                        writer.write(paragraph);
-                                        paragraphCount++;
-                                    }
-                                }
+                            String paragraph = paragraphs[i];
+                            if (paragraph.length() > 3 && // skip empty paragraph
+                                    !(paragraph.startsWith("Art")) && // skip description of art in summary
+                                    !(paragraph.startsWith(" Art")) && // skip description of art in summary
+                                    !paragraph.startsWith("Leave a comment") // skip "Leave a comment"
+                            ) {
+                                System.out.println("Writing: " + paragraph);
+                                FileUtils.writeStringToFile(new File(paragraphFile), paragraph, "UTF-8");
+                                paragraphCount++;
                             }
+
                         }
+                    }
                 }
             }
         } catch (
