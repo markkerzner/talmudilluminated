0066fa35131ba5990573726f60e4e2f2332bc0e7
Mark Kerzner <mark@elephantscale.com>
2023-05-19 00:53:06 -0500

    paragraphs for indexing

--- src/main/java/moses_ai/PrepareTextForIndexing.java
+++ src/main/java/moses_ai/PrepareTextForIndexing.java
@@ -11,7 +11,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class PrepareTextForIndexing {
-    private static final String INPUT_DIR = "./content";
+    private static final String INPUT_DIR = "./site";
+
+    // private static final String INPUT_DIR = "./content";
     private static final String OUTPUT_DIR = "./text-for-indexing";
 
     public static void main(String[] args) {
@@ -19,24 +21,42 @@ public class PrepareTextForIndexing {
             Files.createDirectories(Paths.get(OUTPUT_DIR)); // Create output directory if it does not exist
 
             File folder = new File(INPUT_DIR);
-            File[] listOfFiles = folder.listFiles();
+            File[] listOfDir = folder.listFiles();
 
-            for (File file : listOfFiles) {
-                if (file.isFile() && file.getName().endsWith(".txt")) {
-                    Document doc = Jsoup.parse(file, "UTF-8");
-                    String text = doc.body().text(); // extract text
-                    String[] paragraphs = text.split("\\n+"); // split into paragraphs
+            for (File dir : listOfDir) {
+                if (dir.isDirectory()) {
+                    File[] listOfFiles = dir.listFiles();
+                    for (File file : listOfFiles)
+                        if (file.isFile() && file.getName().endsWith(".html")) {
+                            Document doc = Jsoup.parse(file, "UTF-8");
+                            String htmlWithLineBreaks = doc.html().replaceAll("(?i)<br[^>]*>", "br2n");
+                            Document docWithLineBreaks = Jsoup.parse(htmlWithLineBreaks, "UTF-8");
+                            String text = docWithLineBreaks.body().text(); // extract text
+                            text = text.replaceAll("br2n", "\n");
+                            String[] paragraphs = text.split("\n+"); // split into paragraphs
 
-                    // output each paragraph into a separate file in the output directory
-                    for (int i = 0; i < paragraphs.length; i++) {
-                        String paragraphFile = OUTPUT_DIR + "/" + file.getName() + "-paragraph-" + i + ".txt";
-                        try (BufferedWriter writer = new BufferedWriter(new FileWriter(paragraphFile))) {
-                            writer.write(paragraphs[i]);
+                            // output each paragraph into a separate file in the output director
+                            int paragraphCount = 0;
+                            for (int i = 0; i < paragraphs.length; i++) {
+                                String paragraphFile = OUTPUT_DIR + "/" + file.getName() + "-paragraph-" + paragraphCount + ".txt";
+                                try (BufferedWriter writer = new BufferedWriter(new FileWriter(paragraphFile))) {
+                                    String paragraph = paragraphs[i];
+                                    if (paragraph.length() > 3 && // skip empty paragraph
+                                            !(paragraph.startsWith("Art")) && // skip description of art in summary
+                                            !(paragraph.startsWith(" Art")) && // skip description of art in summary
+                                            !paragraph.startsWith("Leave a comment") // skip "Leave a comment"
+                                    ) {
+                                        System.out.println("Writing: " + paragraph);
+                                        writer.write(paragraph);
+                                        paragraphCount++;
+                                    }
+                                }
+                            }
                         }
-                    }
                 }
             }
-        } catch (IOException e) {
+        } catch (
+                IOException e) {
             e.printStackTrace();
         }
     }
